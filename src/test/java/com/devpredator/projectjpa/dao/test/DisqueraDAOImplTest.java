7915cf711e671d43e3d9c3a1f2636c9c0bf9c324
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2020-10-18 14:56:37 -0500

    Nombre del Proyecto: Project-JPA Descripción: Se integra la funcionalidad de FetchType.EAGER y FetchType.LAZY en el Entity de Genero para las transacciones de JPA y consultar en cascada, así como realizar las transacciones de PERSIST, MERGE y REMOVE para la anotación @Cascade.

---
+++ src/test/java/com/devpredator/projectjpa/dao/test/DisqueraDAOImplTest.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package com.devpredator.projectjpa.dao.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.devpredator.projectjpa.dao.DisqueraDAO;
+import com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl;
+import com.devpredator.projectjpa.entity.Disquera;
+
+/**
+ * @author 4PF28LA_2004
+ *
+ */
+class DisqueraDAOImplTest {
+
+	private DisqueraDAO disqueraDAO = new DisqueraDAOImpl();
+	
+	/**
+	 * Test method for {@link com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl#guardar(com.devpredator.projectjpa.entity.Disquera)}.
+	 */
+	@Test
+	void testGuardar() {
+		
+		Disquera disquera = new Disquera();
+		disquera.setDescripcion("Elektrik");
+		disquera.setFechaCreacion(LocalDateTime.now());
+		disquera.setEstatus(true);
+		
+		this.disqueraDAO.guardar(disquera);
+		
+	}
+
+	/**
+	 * Test method for {@link com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl#actualizar(com.devpredator.projectjpa.entity.Disquera)}.
+	 */
+	@Test
+	void testActualizar() {
+		Disquera disqueraConsultada = this.disqueraDAO.consultarById(9L);
+		
+		disqueraConsultada.setDescripcion("Disquera IronMaiden");
+		
+		this.disqueraDAO.actualizar(disqueraConsultada);
+	}
+
+	/**
+	 * Test method for {@link com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl#eliminar(com.devpredator.projectjpa.entity.Disquera)}.
+	 */
+	@Test
+	void testEliminar() {
+		
+		Long id = 9L;
+		
+		this.disqueraDAO.eliminar(id);
+		
+	}
+
+	/**
+	 * Test method for {@link com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl#consultar()}.
+	 */
+	@Test
+	void testConsultar() {
+		
+		List<Disquera> disquerasConsultadas = this.disqueraDAO.consultar();
+		
+		assertTrue(disquerasConsultadas.size() > 0);
+		
+		disquerasConsultadas.forEach(disquera -> {
+			System.out.println("Disquera: " + disquera.getDescripcion());
+		});
+	}
+
+	/**
+	 * Test method for {@link com.devpredator.projectjpa.dao.impl.DisqueraDAOImpl#consultarById(java.lang.Long)}.
+	 */
+	@Test
+	void testConsultarById() {
+		Disquera disquera = this.disqueraDAO.consultarById(10L);
+		
+		assertNotNull(disquera);
+		
+		System.out.println("Disquera: " + disquera.getDescripcion());
+	}
+	
+	@Test
+	void testConsultarByDescripcionJPQL() {
+	
+		Disquera disqueraConsultada = this.disqueraDAO.consultarByDescripcionJPQL("Elektrik");
+		
+		assertNotNull(disqueraConsultada);
+		
+		System.out.println("Disquera by descripción: " + disqueraConsultada);
+	}
+	
+	@Test
+	void testConsultarByDescripcionNative() {
+		Disquera disqueraConsultada = this.disqueraDAO.consultarByDescripcionNative("Elektrik");
+		
+		assertNotNull(disqueraConsultada);
+		
+		System.out.println("Disquera by descripción: " + disqueraConsultada);
+	}
+
+}
